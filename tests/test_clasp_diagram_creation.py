ee558195874ce7c3521cd65c9f281ae8eaafa2dc
Pedro Olivares <pedro9olivares@gmail.com>
2025-06-09 20:47:42 -0600

    Added tests for creating clasp diagrams from array

--- tests/test_clasp_diagram_creation.py
+++ tests/test_clasp_diagram_creation.py
@@ -23,8 +23,8 @@ def test_chord_for_array_is_hashable():
     chord_set = {chord}
     assert chord in chord_set
 
-# =============== from_matrix() basic creation validation ===============
-# --- tuple of ChordForMatrix validation ---
+# =============== creation via matrix validation ===============
+# --- Creation validation ---
 def test_none_matrix_raises_value_error():
     with pytest.raises(ValueError, match="matrix argument is None"):
         ClaspDiagram.from_matrix(matrix=None)
@@ -33,7 +33,7 @@ def test_non_tuple_matrix_raises_type_error():
     with pytest.raises(TypeError, match="matrix argument must be a tuple of ChordForMatrix instances"):
         ClaspDiagram.from_matrix(matrix=[1, 2, 3])  # (passing list instead of tuple)
 
-def test_non_chord_element_raises_type_error():
+def test_non_chord_element_raises_type_error_from_matrix():
     bad_matrix = (1, 2, 3)
     with pytest.raises(TypeError, match="All elements of the tuple matrix must be of type ChordForMatrix"):
         ClaspDiagram.from_matrix(matrix=bad_matrix)
@@ -81,11 +81,72 @@ def test_invalid_points_raises_value_error():
     with pytest.raises(ValueError, match=r"Invalid start/end points"):
         ClaspDiagram.from_matrix(matrix=bad_matrix)
 
+"""
+TODO: test this
 # --- Happy path ---
-def test_valid_matrix_creates_clasp_diagram():
+def test_happy_path_from_matrix():
     matrix = make_valid_matrix(4)
     clasp = ClaspDiagram.from_matrix(matrix=matrix)
     assert clasp.matrix == matrix
+ """
+
+# =============== creation via array validation ===============
+# --- Creation validation ---
+def test_none_array_raises_value_error():
+    with pytest.raises(ValueError, match="array argument is None"):
+        ClaspDiagram.from_array(array=None)
+
+def test_non_list_raises_type_error():
+    with pytest.raises(TypeError, match="array argument must be a list of ChordForArray instances"):
+        ClaspDiagram.from_array(array=(1, 2, 3))  # (passing tuple instead of list)
+
+def test_non_chord_element_raises_type_error_from_array():
+    bad_array = [1, 2, 3]
+    with pytest.raises(TypeError, match="All elements of the list must be of type ChordForArray"):
+        ClaspDiagram.from_array(array=bad_array)
+
+def test_validate_odd_length_raises_value_error():
+    with pytest.raises(ValueError, match="Array must contain an even number of ChordForArray instances."):
+        ClaspDiagram.from_array([ChordForArray(1, '+', 1)])
+
+# --- Content validation ---
+def test_validate_non_double_occurrence_raises():
+    chord1 = ChordForArray(1, '+', 2)
+    chord2 = ChordForArray(2, '+', 1)
+    arr = [chord1, chord2, chord1, ChordForArray(2, '+', 1)]  # chord2 is not the same object in memory
+    with pytest.raises(ValueError, match="do not appear exactly twice"):
+        ClaspDiagram.from_array(arr)
+
+def test_validate_sign_raises():
+    chord1 = ChordForArray(1, '*', 2)
+    chord2 = ChordForArray(2, '+', 1)
+    arr = [chord1, chord1, chord2, chord2]
+    with pytest.raises(ValueError, match="Invalid sign encountered"):
+        ClaspDiagram.from_array(arr)
+
+def test_validate_chord_idx_raises():
+    chord1 = ChordForArray(2, '+', 2)
+    chord2 = ChordForArray(3, '+', 1)
+    arr = [chord1, chord1, chord2, chord2]
+    with pytest.raises(ValueError, match="Invalid chord_idx encountered"):
+        ClaspDiagram.from_array(arr)
+
+def test_validate_height_raises():
+    chord1 = ChordForArray(1, '+', 3)
+    chord2 = ChordForArray(2, '+', 1)
+    arr = [chord1, chord1, chord2, chord2]
+    with pytest.raises(ValueError, match="Invalid height encountered"):
+        ClaspDiagram.from_array(arr)
+
+"""
+TODO: test this
+def test_happy_path_from_array():
+    chord1 = make_chord(sign='+', chord_idx=1, height=1)
+    chord2 = make_chord(sign='-', chord_idx=2, height=2)
+    arr = [chord1, chord1, chord2, chord2]
+    # Should not raise
+    validate_clasp_array(arr)
+"""
 
 # TODO: =============== ClaspDiagram hashability validation ===============
 
