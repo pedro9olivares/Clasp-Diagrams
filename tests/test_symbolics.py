13064abd3513af6fa15625ca7daa90b13050a61f
Pedro Olivares <pedro9olivares@gmail.com>
2025-06-17 21:04:10 -0600

    Removed print statement

--- tests/test_symbolics.py
+++ tests/test_symbolics.py
@@ -1,6 +1,7 @@
 from hypothesis import given, strategies as st
 from clasp_diagrams.symbolics import get_l_matrix
 from clasp_diagrams.objects import ClaspDiagram, ChordForMatrix
+from clasp_diagrams.utils import matrix_chords_intersect
 import numpy as np
 import sympy as sp
 
@@ -10,12 +11,31 @@ import sympy as sp
 # It uses the SD-matrix, which in turn uses the LE-matrix, so testing the alexander polynomial implicitly tests all previous matrices.
 
 # =============== symbolics.py standalone testing ===============
-def test_get_l_matrix():
-    # TODO: define and calculate by hand a complicated L-matrix
-    clasp_matrix = () # <- Change this
-    expected_l_matrix = np.array([[0,0],  # <- Change this
-                                  [0,0]])
-    calculated_l_matrix = get_l_matrix(clasp_matrix=clasp_matrix) # <- Change this
+def test_get_l_matrix1():
+    clasp_matrix = (ChordForMatrix(0, 3, '+', 3),
+                    ChordForMatrix(1, 5, '+', 2),
+                    ChordForMatrix(2, 4, '+', 1))
+    
+    expected_l_matrix = np.array([[0,1,1],
+                                  [0,0,0],
+                                  [0,0,0]])
+    
+    calculated_l_matrix = get_l_matrix(clasp_matrix=clasp_matrix)
+    #print(f"Calculated L matrix: \n{calculated_l_matrix}")
+    
+    assert np.array_equal(expected_l_matrix, calculated_l_matrix), f"L matrix mismatch: {calculated_l_matrix} != {expected_l_matrix}"
+
+def test_get_l_matrix2():
+    clasp_matrix = (ChordForMatrix(0, 3, '+', 1),
+                    ChordForMatrix(1, 5, '+', 2),
+                    ChordForMatrix(2, 4, '+', 3))
+    
+    expected_l_matrix = np.array([[0,0,0],
+                                  [-1,0,0],
+                                  [-1,0,0]])
+    
+    calculated_l_matrix = get_l_matrix(clasp_matrix=clasp_matrix)
+    #print(f"Calculated L matrix: \n{calculated_l_matrix}")
     
     assert np.array_equal(expected_l_matrix, calculated_l_matrix), f"L matrix mismatch: {calculated_l_matrix} != {expected_l_matrix}"
 
@@ -30,33 +50,30 @@ def test_get_alexander_polynomial():
     alpo_6_2 = 1 - 3*t + 3*t**2 - 3*t**3 + t**4
 
     # Caveat: instead of testing get_alexander_polynomial(), we test ClaspDiagram.alexander
-    # TODO: Replace these placeholder matrices with actual chord data
     unknot = ClaspDiagram.from_matrix(matrix=())
-    cd_3_1 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(),
-                                              ChordForMatrix()))
-    cd_4_1 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(),
-                                              ChordForMatrix()))
-    cd_5_1 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(),
-                                              ChordForMatrix(),
-                                              ChordForMatrix()))
-    cd_5_2 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(),
-                                              ChordForMatrix(),
-                                              ChordForMatrix()))
-    cd_6_1 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(),
-                                              ChordForMatrix(),
-                                              ChordForMatrix()))
-    cd_6_2 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(),
-                                              ChordForMatrix(),
-                                              ChordForMatrix()))
+    cd_3_1 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(0, 2, '+', 2),
+                                              ChordForMatrix(1, 3, '+', 1)))
+    cd_4_1 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(0, 2, '-', 2),
+                                              ChordForMatrix(1, 3, '+', 1)))
+    cd_5_1 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(0, 3, '+', 3),
+                                              ChordForMatrix(1, 4, '+', 2),
+                                              ChordForMatrix(2, 5, '+', 1)))
+    cd_5_2 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(0, 3, '+', 3),
+                                              ChordForMatrix(1, 5, '+', 1),
+                                              ChordForMatrix(2, 4, '+', 2)))
+    cd_6_1 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(0, 3, '-', 3),
+                                              ChordForMatrix(1, 5, '+', 2),
+                                              ChordForMatrix(2, 4, '+', 1)))
+    cd_6_2 = ClaspDiagram.from_matrix(matrix=(ChordForMatrix(0, 3, '-', 3),
+                                              ChordForMatrix(1, 4, '+', 2),
+                                              ChordForMatrix(2, 5, '+', 1)))
     
     # Equality is tested up to sign
-    assert sp.simplify(unknot.alexander - alpo_unknot) == 0 or sp.simplify(-1 * unknot.alexander - alpo_unknot)
-    assert sp.simplify(cd_3_1.alexander - alpo_3_1) == 0 or sp.simplify(-1 * cd_3_1.alexander - alpo_3_1)
-    assert sp.simplify(cd_4_1.alexander - alpo_4_1) == 0 or sp.simplify(-1 * cd_4_1.alexander - alpo_4_1)
-    assert sp.simplify(cd_5_1.alexander - alpo_5_1) == 0 or sp.simplify(-1 * cd_5_1.alexander - alpo_5_1)
-    assert sp.simplify(cd_5_2.alexander - alpo_5_2) == 0 or sp.simplify(-1 * cd_5_2.alexander - alpo_5_2)
-    assert sp.simplify(cd_6_1.alexander - alpo_6_1) == 0 or sp.simplify(-1 * cd_6_1.alexander - alpo_6_1)
-    assert sp.simplify(cd_6_2.alexander - alpo_6_2) == 0 or sp.simplify(-1 * cd_6_2.alexander - alpo_6_2)
-    
-    
+    assert sp.simplify(unknot.alexander - alpo_unknot) == 0 or sp.simplify(-1 * unknot.alexander - alpo_unknot) == 0
+    assert sp.simplify(cd_3_1.alexander - alpo_3_1) == 0 or sp.simplify(-1 * cd_3_1.alexander - alpo_3_1) == 0
+    assert sp.simplify(cd_4_1.alexander - alpo_4_1) == 0 or sp.simplify(-1 * cd_4_1.alexander - alpo_4_1) == 0
+    assert sp.simplify(cd_5_1.alexander - alpo_5_1) == 0 or sp.simplify(-1 * cd_5_1.alexander - alpo_5_1) == 0
+    assert sp.simplify(cd_5_2.alexander - alpo_5_2) == 0 or sp.simplify(-1 * cd_5_2.alexander - alpo_5_2) == 0
+    assert sp.simplify(cd_6_1.alexander - alpo_6_1) == 0 or sp.simplify(-1 * cd_6_1.alexander - alpo_6_1) == 0
+    assert sp.simplify(cd_6_2.alexander - alpo_6_2) == 0 or sp.simplify(-1 * cd_6_2.alexander - alpo_6_2) == 0
     
