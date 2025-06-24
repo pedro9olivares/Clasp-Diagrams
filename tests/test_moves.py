801dae43a05deaf08dfde8eac3becbde356b0a35
Pedro Olivares <pedro9olivares@gmail.com>
2025-06-23 23:04:50 -0600

    Move testing template

---
+++ tests/test_moves.py
@@ -0,0 +1,26 @@
+from clasp_diagrams.objects import ChordForMatrix, ClaspDiagram
+
+# TODO
+
+# ==================== move A: exchange_heights ====================
+def test_A_raises_errors():
+    # Need to check that errors are raised: unit testing by examples
+    raise NotImplementedError()
+
+def test_A_expected():
+    # Strategy 1: Take a clasp diagram and apply the move. The check passes if the
+    # alexander polynomial doesnt change and if the new clasp diagram equals the expected.
+    raise NotImplementedError()
+
+def test_A_return():
+    # Strategy 2: Each move has an inverse. So, test move and then move inverse, should obtain the same clasp diagram.
+    raise NotImplementedError()
+
+def test_A_boundaries():
+    # Also need to check edge cases (valid moves on boundaries)
+    raise NotImplementedError()
+
+def test_A_property():
+    # Also would like to check on several clasp diagrams and on several indexes
+    raise NotImplementedError()
+
