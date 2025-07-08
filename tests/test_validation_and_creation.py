716e39567fd5cd6760c22f9b66925c014faf5c9f
Pedro Olivares <pedro9olivares@gmail.com>
2025-07-08 15:07:10 -0600

    Fixed validation tests

--- tests/test_validation_and_creation.py
+++ tests/test_validation_and_creation.py
@@ -2,6 +2,7 @@ import pytest
 from clasp_diagrams.objects import ChordForMatrix, ChordForArray, ClaspDiagram
 from clasp_diagrams.generators import random_valid_matrix, random_valid_array
 from hypothesis import given, settings, strategies as st
+from clasp_diagrams.validators import ClaspDiagramCreationError
 
 # =============== chord hashability validation ===============
 def test_chord_for_matrix_is_hashable():
@@ -17,7 +18,7 @@ def test_chord_for_array_is_hashable():
 # =============== creation via matrix validation ===============
 # --- Creation validation ---
 def test_none_matrix_raises_value_error():
-    with pytest.raises(ValueError, match="matrix argument is None"):
+    with pytest.raises(ClaspDiagramCreationError, match="matrix argument is None"):
         ClaspDiagram.from_matrix(matrix=None)
 
 def test_non_tuple_matrix_raises_type_error():
@@ -35,7 +36,7 @@ def test_non_unique_heights_raises_value_error():
         ChordForMatrix(0, 1, '+', 1),
         ChordForMatrix(2, 3, '-', 5)
     )
-    with pytest.raises(ValueError, match="The heights are invalid"):
+    with pytest.raises(ClaspDiagramCreationError, match="The heights are invalid"):
         ClaspDiagram.from_matrix(matrix=bad_matrix)
 
 # --- Sign validation ---
@@ -43,7 +44,7 @@ def test_invalid_sign_raises_value_error():
     bad_matrix = (
         ChordForMatrix(0, 1, 'x', 1),
     )
-    with pytest.raises(ValueError, match="Invalid sign encountered in"):
+    with pytest.raises(ClaspDiagramCreationError, match="Invalid sign encountered in"):
         ClaspDiagram.from_matrix(matrix=bad_matrix)
 
 # --- End point validation ---
@@ -51,7 +52,7 @@ def test_invalid_end_point_raises_value_error():
     bad_matrix = (
         ChordForMatrix(1, 1, '+', 1),
     )
-    with pytest.raises(ValueError, match="Invalid start and endpoint in"):
+    with pytest.raises(ClaspDiagramCreationError, match="Invalid start and endpoint in"):
         ClaspDiagram.from_matrix(matrix=bad_matrix)
 
 # --- Start point order validation ---
@@ -60,7 +61,7 @@ def test_start_points_not_strictly_increasing_raises_value_error():
         ChordForMatrix(1, 2, '-', 2),
         ChordForMatrix(0, 3, '+', 1),
     )
-    with pytest.raises(ValueError, match="Invalid order of the start points:"):
+    with pytest.raises(ClaspDiagramCreationError, match="Invalid order of the start points:"):
         ClaspDiagram.from_matrix(matrix=bad_matrix)
 
 # --- Start and end points in range [0, 2n-1] validation ---
@@ -69,7 +70,7 @@ def test_invalid_points_raises_value_error():
         ChordForMatrix(0, 1, '+', 1),
         ChordForMatrix(4, 5, '-', 2)
     )
-    with pytest.raises(ValueError, match="Invalid start/end points"):
+    with pytest.raises(ClaspDiagramCreationError, match="Invalid start/end points"):
         ClaspDiagram.from_matrix(matrix=bad_matrix)
 
 
@@ -85,7 +86,7 @@ def test_creation_from_matrix(n):
 # =============== creation via array validation ===============
 # --- Creation validation ---
 def test_none_array_raises_value_error():
-    with pytest.raises(ValueError, match="array argument is None"):
+    with pytest.raises(ClaspDiagramCreationError, match="array argument is None"):
         ClaspDiagram.from_array(array=None)
 
 def test_non_list_raises_type_error():
@@ -98,7 +99,7 @@ def test_non_chord_element_raises_type_error_from_array():
         ClaspDiagram.from_array(array=bad_array)
 
 def test_validate_odd_length_raises_value_error():
-    with pytest.raises(ValueError, match="Array must contain an even number of ChordForArray instances."):
+    with pytest.raises(ClaspDiagramCreationError, match="Array must contain an even number of ChordForArray instances."):
         ClaspDiagram.from_array([ChordForArray(1, '+', 1)])
 
 # --- Content validation ---
@@ -106,35 +107,35 @@ def test_validate_non_double_occurrence_raises():
     chord1 = ChordForArray(1, '+', 2)
     chord2 = ChordForArray(2, '+', 1)
     arr = [chord1, chord2, chord1, ChordForArray(2, '+', 1)]  # chord2 is not the same object in memory
-    with pytest.raises(ValueError, match="do not appear exactly twice"):
+    with pytest.raises(ClaspDiagramCreationError, match="do not appear exactly twice"):
         ClaspDiagram.from_array(arr)
 
 def test_validate_sign_raises():
     chord1 = ChordForArray(1, '*', 2)
     chord2 = ChordForArray(2, '+', 1)
     arr = [chord1, chord1, chord2, chord2]
-    with pytest.raises(ValueError, match="Invalid sign encountered"):
+    with pytest.raises(ClaspDiagramCreationError, match="Invalid sign encountered"):
         ClaspDiagram.from_array(arr)
 
 def test_validate_chord_idx_raises():
     chord1 = ChordForArray(2, '+', 2)
     chord2 = ChordForArray(3, '+', 1)
     arr = [chord1, chord1, chord2, chord2]
-    with pytest.raises(ValueError, match="Invalid chord_idx encountered"):
+    with pytest.raises(ClaspDiagramCreationError, match="Invalid chord_idx encountered"):
         ClaspDiagram.from_array(arr)
 
 def test_validate_height_raises():
     chord1 = ChordForArray(1, '+', 3)
     chord2 = ChordForArray(2, '+', 1)
     arr = [chord1, chord1, chord2, chord2]
-    with pytest.raises(ValueError, match="Invalid height encountered"):
+    with pytest.raises(ClaspDiagramCreationError, match="Invalid height encountered"):
         ClaspDiagram.from_array(arr)
 
 def test_validate_chord_idx_raises2():
     chord1 = ChordForArray(2, '+', 2)
     chord2 = ChordForArray(1, '+', 1)
     arr = [chord1, chord1, chord2, chord2]
-    with pytest.raises(ValueError, match="Invalid ordering of chord idxs:"):
+    with pytest.raises(ClaspDiagramCreationError, match="Invalid ordering of chord idxs:"):
         ClaspDiagram.from_array(arr)
 
 
